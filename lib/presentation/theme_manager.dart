9497a2c2ee4602ff537392530962af6473e51e8d
Jean <acalto2@gmail.com>
2023-03-18 22:37:25 +0100

    10

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,16 +1,23 @@
 import 'package:cmp_adv_flt_udemy/presentation/color_manager.dart';
 import 'package:flutter/material.dart';
 
+import 'values_manager.dart';
+
 ThemeData getApplicationTheme() {
   return ThemeData(
 //main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-
-    disabledColor: ColorManager.grey1,
-    accentColor: ColorManager.grey,
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
+      accentColor: ColorManager.grey,
 //card view Theme:
+
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ),
 //Appbar theme
 
 //button theme
@@ -18,5 +25,5 @@ ThemeData getApplicationTheme() {
 //text theme
 
 //input decoration theme
-  );
+      );
 }
