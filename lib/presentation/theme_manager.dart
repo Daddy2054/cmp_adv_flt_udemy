6f8e73965dcac2ecc6458d5c9328eedf139ad826
Jean <acalto2@gmail.com>
2023-03-18 22:56:53 +0100

    13

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -8,57 +8,73 @@ import 'values_manager.dart';
 ThemeData getApplicationTheme() {
   return ThemeData(
 //main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
-    accentColor: ColorManager.grey,
-    //ripple color
-    splashColor: ColorManager.primaryOpacity70,
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
+      accentColor: ColorManager.grey,
+      //ripple color
+      splashColor: ColorManager.primaryOpacity70,
 //card view Theme:
 
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      shadowColor: ColorManager.grey,
-      elevation: AppSize.s4,
-    ),
-//Appbar theme
-    appBarTheme: AppBarTheme(
-      centerTitle: true,
-      color: ColorManager.white,
-      elevation: AppSize.s4,
-      shadowColor: ColorManager.primaryOpacity70,
-      titleTextStyle: getRegularStyle(
+      cardTheme: CardTheme(
         color: ColorManager.white,
-        fontSize: FontSize.s16,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
       ),
-    ),
+//Appbar theme
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.white,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s16,
+        ),
+      ),
 
 //button theme
-    buttonTheme: ButtonThemeData(
-      shape: StadiumBorder(),
-      disabledColor: ColorManager.grey1,
-      buttonColor: ColorManager.primary,
-      splashColor: ColorManager.primaryOpacity70,
-    ),
+      buttonTheme: ButtonThemeData(
+        shape: StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.primaryOpacity70,
+      ),
 
-    //elevated button theme
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        textStyle: getRegularStyle(
-          color: ColorManager.white,
-        ),
-        primary: ColorManager.primary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            AppSize.s12,
+      //elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: getRegularStyle(
+            color: ColorManager.white,
+          ),
+          primary: ColorManager.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              AppSize.s12,
+            ),
           ),
         ),
       ),
-    ),
-    
-//text theme
 
+//text theme
+      textTheme: TextTheme(
+        headline1: getSemiBoldStyle(
+          color: ColorManager.darkGrey,
+          fontSize: FontSize.s16,
+        ),
+        subtitle1: getMediumStyle(
+          color: ColorManager.lightGrey,
+          fontSize: FontSize.s14,
+        ),
+        caption: getRegularStyle(
+          color: ColorManager.grey1,
+        ),
+        bodyText1: getRegularStyle(
+          color: ColorManager.grey,
+        ),
+      ),
+      
 //input decoration theme
-  );
+      );
 }
