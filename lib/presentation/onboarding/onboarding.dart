4667310fee1191ad7c80e4952fe50c2142813fe9
Jean <acalto2@gmail.com>
2023-03-20 23:56:38 +0100

    37

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -218,14 +218,4 @@ class OnBoardingPage extends StatelessWidget {
   }
 }
 
-class SliderObject {
-  String title;
-  String subTitle;
-  String image;
 
-  SliderObject(
-    this.title,
-    this.subTitle,
-    this.image,
-  );
-}
--- lib/presentation/onboarding/onboarding_viewmodel.dart
+++ lib/presentation/onboarding/onboarding_viewmodel.dart
@@ -1,10 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
+import '/domain/model.dart';
+
 import '../base/baseviewmodel.dart';
 
-class OnBoardingViewMode extends BaseViewModel
+class OnBoardingViewModel extends BaseViewModel
     with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
 //stream controllers
 
-
+  final StreamController _streamController =
+      StreamController<SlideViewObject>();
   @override
   void dispose() {
     // TODO: implement dispose
@@ -14,21 +20,29 @@ class OnBoardingViewMode extends BaseViewModel
   void start() {
     // TODO: implement start
   }
-  
+
   @override
   void goNext() {
     // TODO: implement goNext
   }
-  
+
   @override
   void goPrevious() {
     // TODO: implement goPrevious
   }
-  
+
   @override
   void onPageChanged(int index) {
     // TODO: implement onPageChanged
   }
+  
+  @override
+  // TODO: implement inputSliderViewObject
+  Sink get inputSliderViewObject => throw UnimplementedError();
+  
+  @override
+  // TODO: implement outputSliderViewObject
+  Stream<SlideViewObject> get outputSliderViewObject => throw UnimplementedError();
 }
 
 // inputs mean the orders that our view model will receive from our view
@@ -36,9 +50,23 @@ abstract class OnBoardingViewModelInputs {
   void goNext(); //when user clicks on right arrow
   void goPrevious(); //when user clicks on left arrow
   void onPageChanged(int index);
+
+  Sink
+      get inputSliderViewObject; //this is the way to add data to the stream ..stream input
 }
 
 // outputs mean data or results taht will be sent from out view model to our view
 abstract class OnBoardingViewModelOutputs {
-  //will be implement it later
+  Stream<SlideViewObject> get outputSliderViewObject;
+}
+
+class SlideViewObject {
+  SliderObject sliderObject;
+  int numOfSlides;
+  int currentIndex;
+  SlideViewObject(
+    this.sliderObject,
+    this.numOfSlides,
+    this.currentIndex,
+  );
 }
