a10792df13dbdb1ab1c7b0c78a79f2ef8891bd0d
Jean <acalto2@gmail.com>
2023-03-22 23:27:26 +0100

    52

---
+++ lib/data/network/failure.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Failure {
+  int code; //200 or 400
+  String message; // error or success
+  Failure(
+     this.code,
+    this.message,
+  );
+
+  
+}
