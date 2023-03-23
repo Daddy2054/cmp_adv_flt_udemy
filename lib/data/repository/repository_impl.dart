eb191a2f3ef4cf60e6e83e5f1cb159f5a8f5e51b
Jean <acalto2@gmail.com>
2023-03-23 22:06:58 +0100

    61

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -2,44 +2,44 @@ import 'package:dartz/dartz.dart';
 
 import '/data/data_source/remote_data_source.dart';
 import '/data/mapper/mapper.dart';
+import '/data/network/error_handler.dart';
+import '/data/network/failure.dart';
 import '/data/network/network_info.dart';
-import '../../domain/model.dart';
-import '../../domain/repository.dart';
-import '../network/failure.dart';
-import '../request/request.dart';
+import '/data/request/request.dart';
+import '/domain/model.dart';
+import '/domain/repository.dart';
 
 class RepositoryImpl extends Repository {
   RemoteDataSource _remoteDataSource;
   NetworkInfo _networkInfo;
+
   RepositoryImpl(this._remoteDataSource, this._networkInfo);
+
   @override
   Future<Either<Failure, Authentication>> login(
       LoginRequest loginRequest) async {
     if (await _networkInfo.isConnected) {
-      //its safe to call the API
-      final response = await _remoteDataSource.login(loginRequest);
-      if (response.status == 0) //success
-      {
-        //return data (success)
-        //return right
-        return Right(response.toDomain());
-      } else {
-        // return business logic error
-        return Left(
-          Failure(
-            409,
-            response.message ?? 'we have biz logic error from API side',
-          ),
-        );
+      try {
+        // its safe to call the API
+        final response = await _remoteDataSource.login(loginRequest);
+
+        if (response.status == ApiInternalStatus.SUCCESS) // success
+        {
+          // return data (success)
+          // return right
+          return Right(response.toDomain());
+        } else {
+          // return biz logic error
+          // return left
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return (Left(ErrorHandler.handle(error).failure));
       }
     } else {
-      //return connection error
-      return Left(
-        Failure(
-          501,
-          'please check your internet connection',
-        ),
-      );
+      // return connection error
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
