7ceac7d10a059908080b3284363776a974ebfe7a
Jean <acalto2@gmail.com>
2023-03-25 23:19:44 +0100

    78

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,6 +1,7 @@
 import 'package:cmp_adv_flt_udemy/presentation/login/login_viewmodel.dart';
 import 'package:cmp_adv_flt_udemy/presentation/resources/assets_manager.dart';
 import 'package:cmp_adv_flt_udemy/presentation/resources/color_manager.dart';
+import 'package:cmp_adv_flt_udemy/presentation/resources/strings_manager.dart';
 import 'package:cmp_adv_flt_udemy/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -71,9 +72,41 @@ class _LoginViewState extends State<LoginView> {
                       return TextFormField(
                         keyboardType: TextInputType.emailAddress,
                         controller: _userNameController,
+                        decoration: InputDecoration(
+                          hintText: AppStrings.username,
+                          labelText: AppStrings.username,
+                          errorText: (snapshot.data ?? true)
+                              ? null
+                              : AppStrings.usernameError, 
+                        ),
                       );
                     },
                     stream: _viewModel.outputIsUserNameValid,
+                  ),
+                ),
+                  SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<bool>(
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          hintText: AppStrings.password,
+                          labelText: AppStrings.password,
+                          errorText: (snapshot.data ?? true)
+                              ? null
+                              : AppStrings.passwordError, 
+                        ),
+                      );
+                    },
+                    stream: _viewModel.outputIsPasswordValid,
                   ),
                 ),
               ],
