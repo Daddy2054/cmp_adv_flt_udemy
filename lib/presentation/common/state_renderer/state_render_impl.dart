16df089bd0a6f9fb9733e361da45392c750c19e8
Jean <acalto2@gmail.com>
2023-03-30 00:17:37 +0200

    98

---
+++ lib/presentation/common/state_renderer/state_render_impl.dart
@@ -0,0 +1,24 @@
+import '/presentation/common/state_renderer/state_renderer.dart';
+import '/presentation/resources/strings_manager.dart';
+
+abstract class FlowState {
+  StateRendererType getStateRendererType();
+
+  String getMessage();
+}
+
+// Loading State (POPUP, FULL SCREEN)
+
+class LoadingState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+
+  LoadingState({required this.stateRendererType, String? message})
+      : message = message ?? AppStrings.loading;
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
